b0fbc10c5838fa7128bd9257b1164b14ba307ff3
Bill Pliske <bill@sixfiveagency.com>
2018-07-11 16:18:30 -0700

    initial css

--- src/App.js
+++ src/App.js
@@ -1,18 +1,51 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import nancy from './images/nancy.jpg';
+import liz from './images/liz.jpg';
+import yvette from './images/yvette.jpg';
+import bill from './images/bill.jpg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="wrapper">
+        <div className="header">
+          <p className="header-text">
+            Welcome, ya'll.
+          </p>
+        </div>
+        <div className="inner-wrapper">
+          <h1 className="title">it's the fed team!</h1>
+          <p className="intro">There might only be four of us, but when we hit you upside the head with our mad skills, you'll think otherwise.</p>
+
+          <div className="mug-wrapper">
+            <div className="person">
+              <img src={nancy} alt="Nancy" />
+              <p className="name">Nancy</p>
+            </div>
+
+            <div className="person">
+              <img src={liz} alt="Liz" />
+              <p className="name">Liz</p>
+            </div>
+
+            <div className="person">
+              <img src={yvette} alt="Yvette" />
+              <p className="name">Yvette</p>
+            </div>
+
+            <div className="person">
+              <img src={bill} alt="Bill" />
+              <p className="name">Bill</p>
+            </div>
+          </div>
+
+          <div className="footer"><p>Copyright 2018, from the Front End Design team.</p></div>
+        </div>
+
+
+
+
       </div>
     );
   }
