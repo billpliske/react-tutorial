61aaf0ebdaee0887068090455b253af8207b0aeb
Bill Pliske <bill@sixfiveagency.com>
2018-07-11 17:59:25 -0700

    more to do on state

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,12 +2,24 @@ import React, { Component } from "react";
 import "../App.css";
 
 class Header extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { number: 0, color: 0 };
+  }
+
+  addNumber = () => {
+    this.setState({ number: this.state.number + 1 });
+  };
+
   render() {
     return (
       <div className={`header ${this.props.name}`}>
         <p className="header-text">Welcome {this.props.name}</p>
-        <button className="add-button">Add</button>
+        <button className="add-button" onClick={this.addNumber}>
+          Add
+        </button>
         <button className="random-button">Random</button>
+        <p className="number">{this.state.number}</p>
       </div>
     );
   }
