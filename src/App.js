80eb01297d8a523dd4d9099433d421c59f4c726e
Bill Pliske <bill@sixfiveagency.com>
2018-07-12 11:54:31 -0700

    updated

--- src/App.js
+++ src/App.js
@@ -8,39 +8,51 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { number: 4 };
+  }
+
+  addNumber = () => {
+    this.setState({ number: this.state.number + 1 });
+  };
+
   render() {
     return (
       <div className="wrapper">
-        <Header />
-        <div className="inner-wrapper">
-          <h1 className="title">it's the fed team!</h1>
-          <p className="intro">
-            There might only be four of us, but when we hit you upside the head
-            with our mad skills, you'll think otherwise.
-          </p>
+        <Header name="" handleClick={this.addNumber} />
+        <div className="gradient-wrapper">
+          <div className="content-wrapper">
+            <h1 className="title">it's the fed team!</h1>
+            <p className="intro">
+              There might only be <strong>{this.state.number}</strong> of us,
+              but when we hit you upside the head with our mad skills, you'll
+              think otherwise.
+            </p>
 
-          <div className="mug-wrapper">
-            <div className="person">
-              <img src={nancy} alt="Nancy" />
-              <p className="name">Nancy</p>
-            </div>
+            <div className="grid-wrapper">
+              <div className="person">
+                <img src={nancy} alt="Nancy" />
+                <p className="name">Nancy</p>
+              </div>
 
-            <div className="person">
-              <img src={liz} alt="Liz" />
-              <p className="name">Liz</p>
-            </div>
+              <div className="person">
+                <img src={liz} alt="Liz" />
+                <p className="name">Liz</p>
+              </div>
 
-            <div className="person">
-              <img src={yvette} alt="Yvette" />
-              <p className="name">Yvette</p>
-            </div>
+              <div className="person">
+                <img src={yvette} alt="Yvette" />
+                <p className="name">Yvette</p>
+              </div>
 
-            <div className="person">
-              <img src={bill} alt="Bill" />
-              <p className="name">Bill</p>
+              <div className="person">
+                <img src={bill} alt="Bill" />
+                <p className="name">Bill</p>
+              </div>
             </div>
+            <Footer />
           </div>
-          <Footer />
         </div>
       </div>
     );
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,28 +1,16 @@
-import React, { Component } from "react";
+import React from "react";
 import "../App.css";
 
-class Header extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { number: 0, color: 0 };
-  }
-
-  addNumber = () => {
-    this.setState({ number: this.state.number + 1 });
-  };
-
-  render() {
-    return (
-      <div className={`header ${this.props.name}`}>
-        <p className="header-text">Welcome {this.props.name}</p>
-        <button className="add-button" onClick={this.addNumber}>
-          Add
-        </button>
-        <button className="random-button">Random</button>
-        <p className="number">{this.state.number}</p>
-      </div>
-    );
-  }
-}
+const Header = props => {
+  return (
+    <div className={`header ${props.name}`}>
+      <p className="header-text">Welcome {props.name}</p>
+      <button className="add-button" onClick={props.handleClick}>
+        Add
+      </button>
+      <button className="random-button">Random</button>
+    </div>
+  );
+};
 
 export default Header;
