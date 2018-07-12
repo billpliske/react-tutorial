ae45e5c8e6a9148887d60bd350e9b26093952c3f
Bill Pliske <bill@sixfiveagency.com>
2018-07-12 13:35:18 -0700

    made stateless

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,15 @@
-import React, { Component } from "react";
+import React from "react";
 import "../App.css";
 
-class Header extends Component {
-  render() {
-    return (
-      <div className={`header ${this.props.name}`}>
-        <p className="header-text">Welcome {this.props.name}</p>
-        <button className="add-button" onClick={this.props.handleClick}>
-          Add
-        </button>
-      </div>
-    );
-  }
-}
+const Header = props => {
+  return (
+    <div className={`header ${props.name}`}>
+      <p className="header-text">Welcome {props.name}</p>
+      <button className="add-button" onClick={props.handleClick}>
+        Add
+      </button>
+    </div>
+  );
+};
 
 export default Header;
