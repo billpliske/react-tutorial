5d5f650c7cf0d1876ab732e8b237d8b05e923239
Bill Pliske <bill@sixfiveagency.com>
2018-07-12 17:07:10 -0700

    updated

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,11 @@
 import React from "react";
 import "../App.css";
+import { Smile } from "styled-icons/fa-regular/Smile";
 
 const Header = props => {
   return (
     <div className={`header ${props.name}`}>
+      <Smile className="smile" />
       <p className="header-text">Welcome {props.name}</p>
       <button className="add-button" onClick={props.handleClick}>
         Add
